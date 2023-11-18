ec0caf255c0dd7c173e9188b751e3b4c44fe1898
dimnov <dimnov1512@gmail.com>
2023-11-19 00:15:12 +0200

    Change path to styles.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import UserList from "./components/user-list/UserList.jsx";
+import "./styles.css";
 
 function App() {
   return (
