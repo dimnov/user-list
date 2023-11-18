3ba9da50798d670e82ed2e1becc2697636c84f07
dimnov <dimnov1512@gmail.com>
2023-11-18 20:45:42 +0200

    Move Search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import Search from "./components/search/Search.jsx";
 import UserList from "./components/user-list/UserList.jsx";
 
 function App() {
