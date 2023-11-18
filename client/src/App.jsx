8dc01837156dd289b6cb740304b36863594eefc5
dimnov <dimnov1512@gmail.com>
2023-11-18 20:42:07 +0200

    Move table and change to UserListTable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Search from "./components/search/Search.jsx";
-import Table from "./components/table/Table.jsx";
+import UserList from "./components/user-list/UserList.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main className="main">
-        <Search />
-        <Table />
+        <UserList />
       </main>
       <Footer />
     </>
