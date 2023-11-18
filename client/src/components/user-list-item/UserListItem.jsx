6e86202e71d6ab88a91923a7580819bb8fbae9b4
dimnov <dimnov1512@gmail.com>
2023-11-18 22:30:42 +0200

    Add date format

--- client/src/components/user-list-item/UserListItem.jsx
+++ client/src/components/user-list-item/UserListItem.jsx
@@ -1,3 +1,5 @@
+import { formatDate } from "../../utils/dataUtils.js";
+
 export default function UserListItem({
   firstName,
   lastName,
@@ -15,7 +17,7 @@ export default function UserListItem({
       <td>{lastName}</td>
       <td>{email}</td>
       <td>{phoneNumber}</td>
-      <td>{createdAt}</td>
+      <td>{formatDate(createdAt)}</td>
 
       <td className="actions">
         <button className="btn edit-btn" title="Edit">
